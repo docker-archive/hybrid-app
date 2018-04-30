8f901fbfb2707d35c72db504bb70b267348f3d07
Elton Stoneman <elton@sixeyed.com>
2018-04-30 17:10:04 +0100

    Fix DTR environment variable

--- scripts/01-build.sh
+++ scripts/01-build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker image build --tag {DTR_HOST}/dockersamples/hybrid-app-db ../database
-docker image build --tag dockersamples/hybrid-app-web ../java-app
-docker image build --tag dockersamples/hybrid-app-web:v2 ../java-app-v2
-docker image build --tag dockersamples/hybrid-app-api:dotnet ../dotnet-api
+docker image build --tag $DTR_HOST/dockersamples/hybrid-app-db ../database
+docker image build --tag $DTR_HOST/dockersamples/hybrid-app-web ../java-app
+docker image build --tag $DTR_HOST/dockersamples/hybrid-app-web:v2 ../java-app-v2
+docker image build --tag $DTR_HOST/dockersamples/hybrid-app-api:dotnet ../dotnet-api

7498609368e09de0ff018059dde673cd22ffd8f2
Elton Stoneman <elton@sixeyed.com>
2018-04-30 16:59:55 +0100

    Fix build scripts

--- scripts/01-build.sh
+++ scripts/01-build.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
-
-# You need Linux and Windows Docker servers to build the full app.
-# Set the host names in these variables:
-linuxDockerHost=ub1604-01
-windowsDockerHost=win2016-01
-
-# Linux images
-export DOCKER_HOST=$linuxDockerHost
-
-docker image build --tag dockersamples/hybrid-app-db ../database
+docker image build --tag {DTR_HOST}/dockersamples/hybrid-app-db ../database
 docker image build --tag dockersamples/hybrid-app-web ../java-app
 docker image build --tag dockersamples/hybrid-app-web:v2 ../java-app-v2
 docker image build --tag dockersamples/hybrid-app-api:dotnet ../dotnet-api
-
-# Windows images
-export DOCKER_HOST=$windowsDockerHost
-
-docker image build --tag dockersamples/hybrid-app-api:netfx ../netfx-api
-
